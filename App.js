05c017cc85fb9388440bcf183374b281eca921b2
nartco <sebastien.thuillier@hotmail.com>
2020-07-05 01:45:39 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { appLoading, AppLoading } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if(!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}
+  }
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const CategoriesScreen = () => {
+    return (
+        <View style={styles.screen}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoriesScreen
