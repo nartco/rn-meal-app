902684384efa1298d5ba5b46c5ca70ad257d62b7
nartco <sebastien.thuillier@hotmail.com>
2020-07-08 17:45:42 +0200

    passing data to meal detail screen

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -2,22 +2,42 @@ import React from "react";
 import { View, StyleSheet, Text, FlatList } from "react-native";
 
 import { CATEGORIES, MEALS } from "../data/dummy-data";
-import Colors from "../constants/Colors";
-
+import MealItem from "../components/MealItem";
 
 const CategoryMealScreen = props => {
-
-    const renderMealItem = itemData => {
-        return (<View><Text>{itemData.item.title}</Text></View>)
-    }
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity.toUpperCase()}
+        affordability={itemData.item.affordability.toUpperCase()}
+        onSelectMeal={() => {
+            props.navigation.navigate({
+                routeName: "MealDetail",
+                params: {
+                  mealId: itemData.item.id
+                }
+              });
+        }}
+      />
+    );
+  };
 
   const catId = props.navigation.getParam("categoryId");
 
-  const displayMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
-  
+  const displayMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
   return (
     <View style={styles.screen}>
-      <FlatList data={displayMeals} renderItem={renderMealItem} />
+      <FlatList
+        data={displayMeals}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 };
@@ -36,8 +56,8 @@ CategoryMealScreen.navigationOptions = navigationData => {
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
   return {
-      headerTitle: selectedCategory.title
-  }
+    headerTitle: selectedCategory.title
+  };
 };
 
 export default CategoryMealScreen;
